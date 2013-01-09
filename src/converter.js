5eaae7b6f2dae5b9941c1fea6a9da47ba46c90a9
Hans Joachim Desserud <github@desserud.org>
2013-01-09 20:41:04 +0100

    Add tests to convert a whole timestamp to binary time

--- src/converter.js
+++ src/converter.js
@@ -1,3 +1,10 @@
 function converter(number) {
   return number.toString(2);
 }
+
+function convertTime(timestamp) {
+  hour = converter(timestamp.getHours());
+  minute = converter(timestamp.getMinutes());
+  second = converter(timestamp.getSeconds());
+  return hour + ":" + minute + ":" + second;
+}
--- test/converter_test.js
+++ test/converter_test.js
@@ -2,4 +2,13 @@ buster.testCase("Converter", {
   "converts from decimal to binary numbers": function () {
     assert.equals(converter(5), 101);
   },
+
+  "zero is still zero after being converted": function () {
+    assert.equals(converter(0), 0);
+  },
+
+  "convert timestamp to binary time": function () {
+    timestamp = new Date(2013, 1, 1, 2, 3, 4);
+    assert.equals(convertTime(timestamp), "10:11:100");
+  }  
 });
